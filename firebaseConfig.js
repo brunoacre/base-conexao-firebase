b6c9fa1c37264a49e62952041d8087e4f323bb74
Bruno Couto Rezende <bruno.rezende.bd@gmail.com>
2026-09-04 10:58:45 -0300

    Obscure Firebase configuration values
    Acerto nas credenciais expostas

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_arjY0VKgZWKBgxURQsFjgsOQSn793EA",
-  authDomain: "projeto-2026-testes-g9h.firebaseapp.com",
-  projectId: "projeto-2026-testes-g9h",
-  storageBucket: "projeto-2026-testes-g9h.firebasestorage.app",
-  messagingSenderId: "1040992336096",
-  appId: "1:1040992336096:web:50cca827b50da478e83885",
-  measurementId: "G-6LKHPJBC3Z"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
